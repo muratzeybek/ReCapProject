5b0915e12c1eb30e446e770f2e7c23894fcbdc8b
projehesabim@hotmail.com <projehesabim@hotmail.com>
2021-02-24 14:47:10 +0300

    Refactoring

--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -4,18 +4,36 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    class ErrorDataResult<T> : Result, IDataResult<T>
+    // Birkaç farklı default false yapalım. Aslında DataResult la yapabiliriz. 
+    // ma alt yapımızı framework umuzu iyileştiriyoruz. 
+    public class ErrorDataResult<T> : DataResult<T>
     {
-        public ErrorDataResult(T data) : base(false)
+        // Bana bir data bir de mesaj ver.
+        // Ama base a (dataresult)a bu işlemin sonucu datadır. Sonucu false dur. Mesaj şudur de.
+        public ErrorDataResult(T data, string message) : base(data, false, message)
         {
-            Data = data;
+
         }
 
-        public ErrorDataResult(T data, string message) : base(false, message)
+        // hiç mesaj olayına girmek istemiyorsa.
+        public ErrorDataResult(T data) : base(data, false)
         {
-            Data = data;
+
         }
 
-        public T Data { get; }
+        // Çok az kullanacağımız bir versiyon. Sadece mesaj için...
+        // 
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        // Hiçbirşey vermek istemiyorum. Base e default datam budur. Mesaj vermiyorum. 
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
+
     }
+
 }
--- Core/Utilities/Results/SuccessDataResult.cs
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -4,18 +4,60 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    public class SuccessDataResult<T> : Result, IDataResult<T>
+    // Birkaç farklı default True yapalım. Aslında DataResult la yapabiliriz. 
+    // ma alt yapımızı framework umuzu iyileştiriyoruz. 
+    public class SuccessDataResult<T> : DataResult<T>
     {
-        public SuccessDataResult(T data) : base(true)
+        // Bana bir data bir de mesaj ver.
+        // Ama base a (dataresult)a bu işlemin sonucu datadır. Sonucu true dur. Mesaj şudur de.
+        public SuccessDataResult(T data, string message) : base(data, true, message)
         {
-            Data = data;
+
         }
 
-        public SuccessDataResult(T data, string message) : base(true,message)
+        // hiç mesaj olayına girmek istemiyorsa.
+        public SuccessDataResult(T data) : base(data, true)
         {
-            Data = data;
+
         }
 
-        public T Data { get; }
+        // Çok az kullanacağımız bir versiyon. Sadece mesaj için...
+        // 
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+
+        }
+
+        // Hiçbirşey vermek istemiyorum. Base e default datam budur. Mesaj vermiyorum. 
+        public SuccessDataResult() : base(default, true)
+        {
+
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    public SuccessDataResult(T data) : base(true)
+    //    {
+    //        Data = data;
+    //    }
+
+    //    public SuccessDataResult(T data, string message) : base(true, message)
+    //    {
+    //        Data = data;
+    //    }
+
+    //    public T Data { get; }
+    //}
 }
