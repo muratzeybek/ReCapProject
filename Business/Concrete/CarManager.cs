827962c8c1f4e70c9e4e9003a809dfd2113813c4
projehesabim@hotmail.com <projehesabim@hotmail.com>
2021-03-11 16:54:29 +0300

    DERS-12 Ödev-3

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation.FluentValidation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -22,9 +23,10 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            ValidationTool.Validate(new CarValidator(), car);
+            // ValidationTool.Validate(new CarValidator(), car);
             _carDal.Add(car);
             return new Result(true, Messages.DataAdded);
         }
